11e5e24f007e637735ee6fb188947e217ea7c66c
EmmanuelRosario <55584405+EmmanuelRosario@users.noreply.github.com>
2019-09-20 12:55:04 +0200

    Tarea 1 PHP
    Mi primer proyecto en PHP

---
+++ tarea1 emmanuel/tarea1.php
@@ -0,0 +1,33 @@
+
+  <!DOCTYPE html>
+  <html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+    <title>tarea1 emmanuel</title>
+  </head>
+  <body>
+    
+  <?php
+    echo "<br></br>";
+  echo  "<h1>Emmanuel Rosario</h1>" ;
+  echo "<br></br>";
+  echo " <h2> Mi Experiencia </h2> ";
+
+  echo  "<p> mi experiencia en el area de informatica empieza en la universidad a los 21 años, actualmente me dedico a dar clases de informatica y colaboro con los docentes como dinamizador </p> ";
+         echo "<br></br>";
+  
+
+  echo " <h3> MIS HOBBIES </h3> ";
+  echo "<p> estudiar, hacer ejercicios y viajar </p>";
+
+
+  echo "<br></br>";
+  echo "<br></br>";
+
+
+    ?>
+  </body>
+  </html>
